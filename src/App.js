e24a53e21cbd99e0749c14075cf16281b0ad5756
santiago herrera <santiherreravelas@gmail.com>
2021-06-17 19:59:39 -0500

    changes for deploy

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import './App.css'
 import React, {useState} from 'react'
-import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
+import {HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
 import Navbar from './components/navbar/navbar'
 import AboutPage from './pages/about'
 import Skills from "./pages/skills/skills";
+
 // import './assets/js/app'
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
     const [toggleSide, setToggleSide] = useState(true)
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       {/*<button onClick={()=>{setToggleSide(!toggleSide)}}>desplegar</button>*/}
       <Navbar show={toggleSide}/>
       <div className={`content ${toggleSide ? '' : 'navUnToggle'} `}>
