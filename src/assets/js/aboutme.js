bd3ed6bc32b010f8660e6dd3dede68133c5887a8
SantiagoHV <sherrerav@correo.udistrital.edu.co>
2022-04-03 21:59:50 -0500

    correction in presentation

--- src/assets/js/aboutme.js
+++ src/assets/js/aboutme.js
@@ -1,10 +1,7 @@
 export default {
     "name": "Santiago Herrera Velásquez",
     "titles": ["Systems engineer student", "Web developer"],
-    "description": "I'm %%% a young student of engineering systems passionate about learning and\n" +
-        "good challenges. Currently, I am a fullstack and mobile web developer in the\n" +
-        "learning process with knowledge in agile frameworks; I really enjoy using and learning the latest technologies, languages\n" +
-        "and frameworks to be in accordance with market trends.",
+    "description": "I'm %%% a young developer and student of systems engineering passionate about learning and good challenges. Currently, I am a fullstack and mobile web developer in the growing process with knowledge in agile frameworks; I am actually looking for a part-time job. Since I enjoy using and learning the latest technologies, languages and frameworks to be in accordance with market trends.",
     "birthdate": new Date(2001, 10, 23),
     "city-from": "Bogotá DC",
     "country-from": "Colombia",
